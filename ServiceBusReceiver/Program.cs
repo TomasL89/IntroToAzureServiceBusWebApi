05a6c52cba917bd2c444ae65c867309d3dbbb49b
Tomas <tomas@polr.software>
2021-03-02 15:56:29 +1100

    Add project files.

---
+++ ServiceBusReceiver/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using ServiceBusShared.Models;
+
+namespace ServiceBusReceiver
+{
+    class Program
+    {
+        const string connectionString = "";
+        private const string queueName = "personqueue";
+        private static IQueueClient queueClient;
+
+
+        static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(connectionString, queueName);
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+
+            await queueClient.CloseAsync();
+        }
+
+        private async static Task ProcessMessagesAsync(Message msg, CancellationToken ct)
+        {
+            var messageData = Encoding.UTF8.GetString(msg.Body);
+            var personModel = JsonSerializer.Deserialize<PersonModel>(messageData);
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Received message");
+            Console.WriteLine($"First Name:{personModel.FirstName}");
+            Console.WriteLine($"Last Name: {personModel.LastName}");
+            Console.WriteLine();
+
+            await queueClient.CompleteAsync(msg.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Message handler exception: {arg.Exception}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ServiceBusSender/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceBusSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceBusSender.Services;
+using ServiceBusShared.Models;
+
+namespace ServiceBusSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IQueueService _queueService;
+
+        public HomeController(ILogger<HomeController> logger, IQueueService queueService)
+        {
+            _logger = logger;
+            _queueService = queueService;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind("FirstName,LastName")] PersonModel person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            try
+            {
+                await _queueService.SendMessageAsync(person, "personqueue");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+    public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
